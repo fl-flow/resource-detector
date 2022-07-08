e6f8c1e6ef93b3a0f0b5b9b82ac85d6053e77a53
choubaodxs <826842697@qq.com>
2022-07-09 02:56:40 +0800

    feat: list_target_process & stop_target_process

--- src/apps/process_res_info/apis.py
+++ src/apps/process_res_info/apis.py
@@ -1,10 +1,14 @@
 import typing
+from datetime import datetime
 
 import psutil
 from fastapi import APIRouter, Depends, HTTPException, exceptions
+from sqlalchemy import desc
 
 from src.apps.process_res_info.schemas import (
     CreateTargetProcessReqModel, CreateTargetProcessResModel,
+    ListTargetProcessResModel,
+    StopTargetProcessReqModel, StopTargetProcessResModel,
 )
 from src.databases.local import dao
 from src.databases.local.db import LocalSession, get_local_db
@@ -27,7 +31,8 @@ def create_target_process(
         raise HTTPException(400, repr(e))
 
     create_data: dict = req_data.dict()
-    identify_id = create_data.get('identify_id')
+    # identify_id = create_data.get('identify_id')
+    identify_id = req_data.identify_id
     if identify_id:
         # existed_target_process: TargetProcess = db.query(TargetProcess).filter(TargetProcess.identify_id == identify_id).first()
         existed_target_process: TargetProcess = dao.retrieve_target_process(db, TargetProcess.identify_id == identify_id)
@@ -37,6 +42,7 @@ def create_target_process(
             existed_target_process.cmdline = p_cmdline
             existed_target_process.status = TargetProcess.StatusChoices.monitoring
             existed_target_process.save(db)
+            setattr(existed_target_process, 'existed', True)
             return existed_target_process
 
     create_data.update({
@@ -44,3 +50,32 @@ def create_target_process(
         'cmdline': p.cmdline(),
     })
     return dao.create_target_process(db, **create_data)
+
+
+@process_res_info_router.get('/target_process/', response_model=typing.List[ListTargetProcessResModel])
+def list_target_process(
+        status: typing.Optional[int] = None,
+        db: LocalSession = Depends(get_local_db),
+):
+    queryset = db.query(TargetProcess)
+    if status:
+        queryset = queryset.filter(TargetProcess.status == status)
+    return queryset.order_by(desc(TargetProcess.updated_at)).all()
+
+
+@process_res_info_router.post('/stop_target_process/', response_model=StopTargetProcessResModel)
+def stop_target_process(
+        req_data: StopTargetProcessReqModel,
+        db: LocalSession = Depends(get_local_db),
+):
+    update_data = {'status': TargetProcess.StatusChoices.stopped, 'updated_at': datetime.now()}
+    queryset = db.query(TargetProcess).filter(TargetProcess.status == TargetProcess.StatusChoices.monitoring)
+    identify_id_update_count = 0
+    pid_update_count = 0
+    if req_data.identify_id:
+        identify_id_update_count = queryset.filter(TargetProcess.identify_id == req_data.identify_id).update(update_data)
+        db.commit()
+    if req_data.pid:
+        pid_update_count = queryset.filter(TargetProcess.pid == req_data.pid).update(update_data)
+        db.commit()
+    return {'pid_update_count': pid_update_count, 'identify_id_update_count': identify_id_update_count}
--- src/apps/process_res_info/schemas.py
+++ src/apps/process_res_info/schemas.py
@@ -12,8 +12,31 @@ class CreateTargetProcessResModel(BaseModel):
     id: int
     identify_id: typing.Optional[str]
     pid: int
-    name: typing.Optional[str]
-    cmdline: typing.Optional[typing.List]
+    name: str
+    cmdline: typing.List
+    existed: typing.Optional[bool]
 
     class Config:
         orm_mode = True
+
+
+class ListTargetProcessResModel(BaseModel):
+    id: int
+    identify_id: typing.Optional[str]
+    pid: int
+    name: str
+    cmdline: typing.List
+    status: int
+
+    class Config:
+        orm_mode = True
+
+
+class StopTargetProcessReqModel(BaseModel):
+    identify_id: typing.Optional[str]
+    pid: typing.Optional[int]
+
+
+class StopTargetProcessResModel(BaseModel):
+    pid_update_count: int
+    identify_id_update_count: int
